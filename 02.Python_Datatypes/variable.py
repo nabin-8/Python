5b3c93fcdb1d3c2ffc2796f994d82f99086ab8af
Nabin Acharya <nabinacharya740@gmail.com>
2024-03-31 20:08:01 +0545

    variables

---
+++ 02.Python_Datatypes/variable.py
@@ -0,0 +1,48 @@
+
+
+a=10
+print(a)
+
+name='soap'
+print(name)
+
+'''
+b
+NameError: name 'b' is not defined
+
+just decleration is not possible in python
+
+'''
+
+# Declare Multiple variables
+x,y,z=5,10,15
+print(x, y, z)
+
+name, price, weight="toothpaste", 10.5,5
+print(price, name, weight)
+
+i,j,k=1,1,1
+# we can also write like this
+i=j=k=1
+
+'''
+Python is dynamically typde programming language
+
+if i type x=10 then it is integer datatype
+x is a variable that will just hold the value belonging to any type of data
+
+x=13.75 this is float type of data
+'''
+
+x=25
+print(x)
+print(type(x))
+x=13.75
+print(x)
+print(type(x))
+x='A'
+print(x)
+print(type(x))
+x=[1,2,3]
+print(x)
+print(type(x))
